11ed54e884ee6bcbcc9ad10dc123532b959ca729
Agung Dwi Lambang G S <agungdlgs@gmail.com>
2020-11-20 03:15:09 +0700

    reset js

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,29 +1,9 @@
 "use strict";
 
 $(function () {
-  function initIsotope() {
-    var $grid = $('.grid');
+  function initIsotope() {}
 
-    if ($grid.length) {
-      var _grid = $grid.isotope({
-        itemSelector: '.grid-item',
-        percentPosition: true,
-        masonry: {
-          columnWidth: '.grid-sizer'
-        }
-      });
-
-      $('.filter-group').on('click', 'a', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        var filterValue = $(this).attr('data-filter');
-
-        _grid.isotope({
-          filter: filterValue
-        });
-      });
-    }
-  }
+  function initNavigationSpy() {}
 
   initIsotope();
 });
