b377958c448676e6b7ed1342f55c99cf75d2d3a5
Agung Dwi Lambang G S <agungdlgs@gmail.com>
2020-11-20 05:36:15 +0700

    end episode 2

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,7 +1,30 @@
 "use strict";
 
 $(function () {
-  function initIsotope() {}
+  function initIsotope() {
+    var $grid = $('.grid');
+
+    if ($grid.length) {
+      var _grid = $grid.isotope({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        masonry: {
+          columnWidth: '.grid-sizer'
+        }
+      });
+
+      $('.filter-box').on('click', 'a', function (e) {
+        e.preventDefault();
+        var filterValue = $(this).attr('data-filter');
+        $('.is-selected').removeClass('is-selected');
+        $(this).addClass('is-selected');
+
+        _grid.isotope({
+          filter: filterValue
+        });
+      });
+    }
+  }
 
   function initNavigationSpy() {}
 
--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 $(function() {
 
     function initIsotope() {
+        const $grid = $('.grid');
 
+        if ($grid.length) {
+            const _grid = $grid.isotope({
+                itemSelector: '.grid-item',
+                percentPosition: true,
+                masonry: {
+                    columnWidth: '.grid-sizer',
+                }
+            });
+
+            $('.filter-box').on('click', 'a', function(e) {
+                e.preventDefault();
+                var filterValue = $( this ).attr('data-filter');
+                $('.is-selected').removeClass('is-selected');
+                $(this).addClass('is-selected');
+                _grid.isotope({ filter: filterValue });
+            })
+        }
     }
 
     function initNavigationSpy() {
